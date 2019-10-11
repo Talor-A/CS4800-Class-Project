3b36e52e15103e127768c989aec5ea15c6ce8b39
ryan <rgskinner@cpp.edu>
2019-10-11 16:27:41 -0700

    ads

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var path = require('path');
+var mysql = require('mysql');
+
+
+  var connection = mysql.createConnection({ host: "199.19.224.212", user: "ryan", password: "password", database: "software"});
+  connection.connect();
+  var v;
+  let sql = `SELECT base64, description FROM dresses`;
+  connection.query(sql, (error, results, fields) => {
+    if (error) { return console.error(error.message);}
+    //console.log(results);
+  });
+
 
 // Example of an API handler
 var usersRouter = require('./routes/users');
@@ -13,9 +25,15 @@ app.use('/users', usersRouter);
 
 // app.use('/', indexRouter);
 
+
 // serve the index.html page
 app.get('/', function(req, res) {
   res.sendFile('pages/index.html' , { root : __dirname});
 });
 
+
+
+
+
 module.exports = app;
+
