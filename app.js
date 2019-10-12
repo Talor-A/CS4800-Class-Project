854208911a83545119697c13926e78fa32ef82a7
ryan <rgskinner@cpp.edu>
2019-10-11 17:42:00 -0700

    images loaded via js, but js doesnt read database yet

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 var express = require('express');
 var path = require('path');
-var mysql = require('mysql');
-
-
-  var connection = mysql.createConnection({ host: "199.19.224.212", user: "ryan", password: "password", database: "software"});
-  connection.connect();
-  var v;
-  let sql = `SELECT base64, description FROM dresses`;
-  connection.query(sql, (error, results, fields) => {
-    if (error) { return console.error(error.message);}
-    //console.log(results);
-  });
 
 
 // Example of an API handler
 var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -22,16 +12,13 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
+//app.use('/', indexRouter);
 
-// app.use('/', indexRouter);
 
-
-// serve the index.html page
+//serve the index.html page
 app.get('/', function(req, res) {
   res.sendFile('pages/index.html' , { root : __dirname});
 });
-
-
 
 
 
