e726e8a44be84aead9491b2036ddf77dbfc0110b
Talor Anderson <ta.anders17@gmail.com>
2019-10-21 00:53:30 -0700

    basic animations working

--- public/javascript/dynamic_images.js
+++ public/javascript/dynamic_images.js
@@ -41,6 +41,8 @@ function initateCarousel() {
 var body = {
   LEG_L: {},
   LEG_R: {},
+  legs: {},
+  torso: {}
 }
 
 SVG.on(document, 'DOMContentLoaded', function () {
@@ -56,52 +58,105 @@ SVG.on(document, 'DOMContentLoaded', function () {
   var draw = SVG('render')
 
   var LEG_R = {
-    "LEG_R_A":"images/SVG/LEGS/SVG/LEG R A.svg",
-    "LEG_R_B":"images/SVG/LEGS/SVG/LEG R B.svg",
-    "LEG_R_C":"images/SVG/LEGS/SVG/LEG R C.svg",
-    "LEG_R_D":"images/SVG/LEGS/SVG/LEG R D.svg",
+    "LEG_R_A": "images/SVG/LEGS/SVG/LEG R A.svg",
+    "LEG_R_B": "images/SVG/LEGS/SVG/LEG R B.svg",
+    "LEG_R_C": "images/SVG/LEGS/SVG/LEG R C.svg",
+    "LEG_R_D": "images/SVG/LEGS/SVG/LEG R D.svg",
+  }
+  var legs_2 = {
+    "legs_2A": "images/SVG/LEGS/SVG/Asset 1.svg",
+    "legs_2B": "images/SVG/LEGS/SVG/Asset 2.svg"
   }
 
-  var requests = Object.entries(LEG_R).map(([id, url]) =>
+  var torso = {
+    "torso_A": "images/SVG/LEGS/SVG/Asset 3.svg",
+    "torso_B": "images/SVG/LEGS/SVG/Asset 4.svg"
+  }
+
+  var legreq = Object.entries(legs_2).map(([id, url]) =>
     fetch(url)
-    .then(res => res.text())
-    .then(text => {
-      // draw.svg() returns a reference to the draw object.
-      // it doesn't return a reference to the shape we just made
-      draw.svg(text)
+      .then(res => res.text())
+      .then(text => {
+        // draw.svg() returns a reference to the draw object.
+        // it doesn't return a reference to the shape we just made
+        draw.svg(text)
 
-      // so find the last shape added to the view and return it
-      var children = draw.children()
+        // so find the last shape added to the view and return it
+        var children = draw.children()
 
-      var thisSvg = children[children.length -1]
-      thisSvg.attr("id", id)
-      return thisSvg
-    })
-    .then(node => {
-      node.hide()
-      body.LEG_R[id] = node
-      console.log(node)
-    })
+        var thisSvg = children[children.length - 1]
+        thisSvg.attr("id", id)
+        return thisSvg
+      })
+      .then(node => {
+        node.hide()
+        body.legs[id] = node
+        console.log(node)
+      })
+  )
+  var torsoreq = Object.entries(torso).map(([id, url]) =>
+    fetch(url)
+      .then(res => res.text())
+      .then(text => {
+        // draw.svg() returns a reference to the draw object.
+        // it doesn't return a reference to the shape we just made
+        draw.svg(text)
+
+        // so find the last shape added to the view and return it
+        var children = draw.children()
+
+        var thisSvg = children[children.length - 1]
+        thisSvg.attr("id", id)
+        return thisSvg
+      })
+      .then(node => {
+        node.hide()
+        body.torso[id] = node
+        console.log(node)
+      })
   )
 
-  Promise.all(requests).then(()=>{
-    console.log("leg loaded.")
-    
-    var pathRA = body.LEG_R.LEG_R_A.select('path.cls-1').first().array().toString()
-    var pathRB = body.LEG_R.LEG_R_C.select('path.cls-1').first().array().toString()
-    console.log(pathRA)
-    console.log(pathRB)
+  var requests = [...legreq, ...torsoreq]
 
-    var legA = draw.path(pathRA)
-    legA.animate(2000, '>', 1000).plot(pathRB)
-    // create path
-    var path = draw.path('M150 0 L75 200 L225 200 Z')
 
-    // animate path
-    path.animate(2000, '>', 1000).plot('M100 0 H190 V90 H100 Z')
+  Promise.all(requests).then(() => {
+    console.log("all svgs loaded.")
+    var tA = body.torso.torso_A
+    var tB = body.torso.torso_B
+
+    var pathTorsoA = tA.select('#Torso').first()
+    var pathTorsoB = tB.select('#Torso').first()
+
+    console.log(pathTorsoA.array().value)
+
+    // var tA = draw.path(pathTorsoA.array().toString())
+    // tA.show()
+    // var tB = draw.path(pathTorsoA.array().toString())
+    // pathTorsoA.animate(2000, '>', 1000).plot(pathTorsoB.array())
+    // // create path
+
+    var leg_A = body.legs.legs_2A
+    var leg_B = body.legs.legs_2B
+    var leftA = leg_A.select('#left').first()
+    var leftB = leg_B.select('#left').first()
+    var rightA = leg_A.select('#right').first()
+    var rightB = leg_B.select('#right').first()
+
+    leg_A.show()
+    // leg_B.show()
+
+    leftA.animate(2000, '>', 0).plot(leftB.array())
+    rightA.animate(2000, '>', 0).plot(rightB.array())
+
+
+
+    // var path = draw.path('M150 0 L75 200 L225 200 Z')
+
+    // // animate path
+    // path.animate(2000, '>', 1000).plot('M100 0 H190 V90 H100 Z')
 
   })
-  
+
 
 
 
