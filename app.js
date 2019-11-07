a86deca33ff98f3029d5970b2c869106389c6e8a
ryan <rgskinner@cpp.edu>
2019-11-06 16:37:11 -0800

    Removed unused files:         user.js (never used)         test.html (kute.js code)         bootstrap.css (loaded from CDN now) 	test svgs (never really needed) 	weddingimage1-5 (loaded from database)
    Moved files:
            SVG images had a redundant directory
            'logo and background' to image folder
    	svg.js and svg.pathmorphing.js to libraies folder
    
    Renamed files:
    	Admin.html -> admin.html
    	dynamic_images.js -> carousel.js
    
    Other:
    	Added backup for database - software_db_backup.sql in root dir
    	Fixed admin console - upload works, removed image upload, changed routes around

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 var express = require('express');
 var path = require('path');
 
-
 // Example of an API handler
-var usersRouter = require('./routes/users');
+
 var apiRouter = require('./routes/API')
+var adminRouter = require('./routes/admin')
 
 var app = express();
-
 // serve everything from the public/ folder as static assets (not changing)
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
+
 app.use('/api', apiRouter);
+app.use('/admin', adminRouter);
 
 //serve the index.html page
 app.get('/', function(req, res) {
   res.sendFile('pages/index.html', { root : '.'});
 });//serve the index.html page
-
 
 
 module.exports = app;
--- routes/API.js
+++ routes/API.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
-
-
 var con = mysql.createConnection({host: "199.19.224.212", user: "ryan", password: "password", database: "software"});
 con.connect();
-
 
 router.get('/dresses', function(req, res, next) {
   con.query("SELECT * FROM dresses", function (err, result, fields) {
@@ -18,39 +13,5 @@ router.get('/dresses', function(req, res, next) {
   //con.end();
 });
 
-var auth = require('http-auth');
-var basic = auth.basic({
-        realm: "Simon Area."
-    }, (username, password, callback) => { 
-        // Custom authentication
-        // Use callback(error) if you want to throw async error.
-        callback(username === "admin" && password === "admin");
-    }
-);
- 
-// Application setup.
-router.use(auth.connect(basic));
- 
-// Setup route.
-router.get('/', (req, res) => {
-    res.send(`Hello from express - ${req.user}! <a href="/api/admin/">Admin Console</a>`);
-});
-
-
-//serve the Admin.html page
-router.get('/Admin', function(req, res) {
-  res.sendFile('pages/Admin.html' , { root : '.'});
-});//serve the Admin.html page
- 
-router.post('/thank', urlencodedParser, function (req, res){
-  var command = "INSERT INTO `dresses`(`base64`, `description`) VALUES (\"" + req.body.base + "\" ,\"" + req.body.description + "\" )"
-  var reply = ""
-  con.query(command, function (err, result, fields) {
-      if (err) throw err;
-      //res.json(result)
-      reply += "Dress added."
-  });
-  res.send(reply);
- });
 
 module.exports = router;
